2bda1434eb720c75e0920aa7e3f9c29c1cae0989
Lilian CAFFIER <krykox@Mac.lan>
2025-01-05 03:51:40 +0100

    Initialization of Prisma ORM :billd_cap:

--- app/(dashboard)/page.tsx
+++ app/(dashboard)/page.tsx
@@ -1,13 +1,25 @@
+import {Table, TableBody, TableCell} from "@/components/ui/table";
+import {prisma} from "@/lib/prisma";
 import { PageParams } from "@/types/next";
 import {Home} from "lucide-react";
 
 export default async function RoutePage(props: PageParams<{}>) {
     
+    const users = await prisma.user.findMany();
     
     return (
         <div>
-            lol
-            <Home />
+            {users.map((user) => {
+                return (
+                    <Table>
+                        <TableBody>
+                            <TableCell>{user.id}</TableCell>
+                            <TableCell>{user.email}</TableCell>
+                            <TableCell>{user.name}</TableCell>
+                        </TableBody>
+                    </Table>
+                )
+            })}
         </div>
     )
 }
