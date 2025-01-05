48dbd0fd9b52daa073a08257c436399a53b37113
Lilian CAFFIER <krykox@Mac.lan>
2025-01-05 03:18:46 +0100

    add lucide react for icons :star2:

--- app/(dashboard)/page.tsx
+++ app/(dashboard)/page.tsx
@@ -1,10 +1,13 @@
 import { PageParams } from "@/types/next";
+import {Home} from "lucide-react";
 
 export default async function RoutePage(props: PageParams<{}>) {
+    
     
     return (
         <div>
             lol
+            <Home />
         </div>
     )
 }
